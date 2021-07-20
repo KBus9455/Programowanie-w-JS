f1ea750f9aee37dccba6965cda413690fdbce1a6
KBus9455 <Katarzyna.Bus9455@gmail.com>
2021-07-20 19:26:53 +0200

    add condition

--- Day6/Zad4/main.js
+++ Day6/Zad4/main.js
@@ -5,56 +5,57 @@ let green = 100;
 let blue = 100;
 
 document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-/*
-const changeColor = (e) => {
-  console.log(e.keyCode, e.which);
-  switch (e.keyCode) {
-    case 38:
-      console.log("dziala");
-      red++;
-      green++;
-      blue++;
-      document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-      break;
-    case 40:
-      red--;
-      green--;
-      blue--;
-      document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-      break;
-  }
-  */
-const changeColor = (e) => {
-  let x = e.keyCode;
 
-  if (x === 38) {
-    if (red < 255 && green < 255 && blue < 255) {
-      red++;
-      green++;
-      blue++;
-      document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+const changeColor = (e) => {
+    console.log(e.keyCode, e.which);
+    switch (e.keyCode) {
+      case 38:
+ //       console.log("dziala");
+        red <= 255 ? red++ : red;
+        green <= 255 ? green++ : green;
+        blue <= 255 ? blue++ : blue;
+        document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        console.log(red);
+        break;
+      case 40:
+        red >=0 ? red-- : red;
+        green >=0 ? green-- : green;
+        blue >=0 ? blue-- : blue;
+        document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        break;
     }
-    console.log(red);
+    /*
+    const changeColor = (e) => {
+      let x = e.keyCode;
 
-  } else if (x === 40) {
-    if (red > 0 && green > 0 && blue >0) {
-      red--;
-      green--;
-      blue--;
-      document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-      console.log(red);
+      if (x === 38) {
+        if (red < 255 && green < 255 && blue < 255) {
+          red++;
+          green++;
+          blue++;
+          document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        }
+        console.log(red);
+
+      } else if (x === 40) {
+        if (red > 0 && green > 0 && blue >0) {
+          red--;
+          green--;
+          blue--;
+          document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+          console.log(red);
+        };
+      };
     };
-  };
-};
+      */
+    // 38 - strzałka do góry
+    // 40 - strzalka w dół
 
-// 38 - strzałka do góry
-// 40 - strzalka w dół
-
-// wciśnięcie klawisza strzałki do góry ma zmieniać kolor (stopniowo) na biały czyli rgb(255,255,255). Wciśniecie klawisza strzałki w dół ma zmienić kolor stopniowo na czarny (docelowo rgb(0,0,0)) 
-//tutaj twój kod
-// wersja 1 - instrukcja if
-// wersja 2 - instrukcja switch
+    // wciśnięcie klawisza strzałki do góry ma zmieniać kolor (stopniowo) na biały czyli rgb(255,255,255). Wciśniecie klawisza strzałki w dół ma zmienić kolor stopniowo na czarny (docelowo rgb(0,0,0)) 
+    //tutaj twój kod
+    // wersja 1 - instrukcja if
+    // wersja 2 - instrukcja switch
 
 
 
-window.addEventListener('keydown', changeColor)
+    window.addEventListener('keydown', changeColor)
