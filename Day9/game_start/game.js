b7ab8cbd20ccd6f7e587980a335e5bb11eba0cf0
KBus9455 <Katarzyna.Bus9455@gmail.com>
2021-07-26 22:29:29 +0200

    add function of start game, player and computer selection, add summary in console

---
+++ Day9/game_start/game.js
@@ -0,0 +1,56 @@
+const hands = [...document.querySelectorAll('img')];
+const letsPlay = document.querySelector('.start');
+const options = ['kamień', 'papier', 'nożyczki'];
+const game = {
+    yourChoice: "",
+    computerChoice: "",
+}
+let gameSummary = {
+    games: 0,
+    wins: 0,
+    looses: 0,
+    draws: 0
+}
+const HandSelection = function () {
+    game.yourChoice = this.dataset.option;
+    console.log("Player " + game.yourChoice);
+    hands.forEach(hand => hand.style.boxShadow = "")
+    this.style.boxShadow = "0 0 4px red"
+}
+
+hands.forEach(function (hand) {
+    hand.addEventListener('click', HandSelection)
+});
+const computerSelection = () => {
+    const index = (Math.floor(Math.random() * hands.length));
+    game.computerChoice = options[index];
+    return game.computerChoice;
+}
+const addSummary = () => {
+    if (game.computerChoice == game.yourChoice) {
+        gameSummary.draws++;
+        console.log("Remis");
+    } else if ((game.computerChoice == "nożyczki" && game.yourChoice == "papier") || (game.computerChoice == "papier" && game.yourChoice == "kamień") || (game.computerChoice == "kamień" && game.yourChoice == "nożyczki")) {
+        gameSummary.looses++;
+              console.log("Przegrałeś");
+    } else {
+        gameSummary.wins++;
+              console.log("Wygraleś");
+    }
+}
+
+const showSummary = ()=>{
+    console.log(`Wygrane: ${gameSummary.wins}, Przegrane: ${gameSummary.looses}, Remisy: ${gameSummary.draws} `)
+}
+const startGame = () => {
+    if (game.yourChoice == "") {
+        alert("Nic nie wybrales");
+        return;
+    }
+    gameSummary.games++;
+    game.computerChoice = computerSelection();
+    console.log("Komputer " + game.computerChoice);
+    addSummary();
+    showSummary();
+}
+letsPlay.addEventListener('click', startGame)
