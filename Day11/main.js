68f431d8b72c535bbee80d0f6c9576905fd8d65b
KBus9455 <Katarzyna.Bus9455@gmail.com>
2021-08-03 21:08:24 +0200

    add changeDots function

--- Day11/main.js
+++ Day11/main.js
@@ -1,15 +1,20 @@
-const images = ['images/img1.jpg','images/img2.jpg','images/img3.jpg'];
+const images = ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg'];
 const texts = ['Pierwszy tekst', 'Drugi tekst', 'Trzeci tekst']
+const dotSpans = [...document.querySelectorAll('span')];
 const img = document.querySelector("img");
 const h1 = document.querySelector('h1');
-
+let time = 1000;
 let i = 1;
-const change = ()=>{
+const changeDots = () => {
+    dotSpans.forEach((span) => span.classList.remove("active"));
+    dotSpans[i].classList.add('active');
+}
+const change = () => {
     img.src = images[i];
     h1.textContent = texts[i];
+    changeDots();
     i++;
-    if (i >= images.length) i=0;
-    
+    if (i >= images.length) i = 0;
 }
 
-setInterval(change, 700)
+setInterval(change, time)
